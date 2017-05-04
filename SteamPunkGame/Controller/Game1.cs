034d9e8e0b2ce48139181dcbd8d22f4efddf34a7
juvekeeper41 <josb6385@740s-pl-114.local>
2017-05-04 09:41:33 -0600

    Created the SteamPunkShooter game

---
+++ SteamPunkGame/Controller/Game1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SteamPunkGame.Model;
+
+namespace SteamPunkGame
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		#region Declaration Section
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		private Player player; 
+
+		// Keyboard states used to determine key presses
+		KeyboardState currentKeyboardState;
+		KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		GamePadState currentGamePadState;
+		GamePadState previousGamePadState;
+
+		#endregion
+
+		// A movement speed for the player
+		float playerMoveSpeed;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			//TODO: use this.Content to load your game content here 
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+#endif
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.DarkCyan);
+
+			// Start drawing
+			spriteBatch.Begin();
+
+			// Draw the Player
+			player.Draw(spriteBatch);
+
+			// Stop drawing
+			spriteBatch.End();
+
+			//TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
